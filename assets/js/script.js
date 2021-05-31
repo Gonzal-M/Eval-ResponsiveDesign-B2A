820a60e15179c1ddd20bc36d4d0feab4cc344579
Clawchette <marie.gonzalez@ynov.com>
2021-05-31 11:16:54 +0200

    burger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-//JS file
+var burgerMenu = document.querySelector('.burger__menu');
+var headerMenu = document.querySelector(".burger__menu__list");
+var headerOverlay = document.querySelector('.menu__overlay');
+var body = document.querySelector('body');
+
+function toggleBurgerMenu(burger, menu, overlay) {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+    overlay.classList.toggle('active');
+    body.classList.toggle('stop__scroll');
+}
+
+burgerMenu.onclick = function () {
+    toggleBurgerMenu(burgerMenu, headerMenu, headerOverlay);
+}
+
+headerOverlay.onclick = function () {
+    toggleBurgerMenu(burgerMenu, headerMenu, headerOverlay);
+}
